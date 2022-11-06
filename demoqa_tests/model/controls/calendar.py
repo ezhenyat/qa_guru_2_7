572eb5dc689937be782866cfb3973a9c1f211f33
ea-timofeev <ezhenyat12@gmail.ocm>
2022-11-06 18:51:37 +0400

    add operational system dependency to calendar fill_by_input

--- demoqa_tests/model/controls/calendar.py
+++ demoqa_tests/model/controls/calendar.py
@@ -1,10 +1,14 @@
+from sys import platform
 from selene.support.shared import browser
 from selenium.webdriver.common.keys import Keys
 from selene import have, command
 
 
 def fill_by_input(element, value):
-    browser.element(element).click().send_keys(Keys.COMMAND + 'a').type(value).press_enter()
+    if platform == 'darwin':
+        browser.element(element).click().send_keys(Keys.COMMAND + 'a').type(value).press_enter()
+    else:
+        browser.element(element).click().send_keys(Keys.CONTROL + 'a').type(value).press_enter()
 
 
 def fill_by_datepicker(element, day, month, year):
@@ -12,4 +16,3 @@ def fill_by_datepicker(element, day, month, year):
     browser.element('.react-datepicker__year-select').send_keys(year)
     browser.element('.react-datepicker__month-select').send_keys(month)
     browser.element(f'.react-datepicker__day--0{day}'f':not(.react-datepicker__day--outside-month)').click()
-
